0f73969eb5dcaf26a3e1259a1d494d53f1da9e0e
pengxs42098 <pengxs42098@hundsun.com>
2023-04-04 13:55:33 +0800

    Initial commit

---
+++ src/main/java/com/qianfeng/magic/gateway/MagicGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.qianfeng.magic.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicGatewayApplication.class, args);
+    }
+
+}
